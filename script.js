98dcf4de74b960dbffafd70653fda84b68ed978b
Greg Schuman <gcschuman@hotmail.com>
2020-09-03 19:52:17 -0700

    Fininshed working on the function to update balance, income, and expense, as well as add it to the DOM.

--- script.js
+++ script.js
@@ -38,7 +38,21 @@ function updateValues(){
 
     const total = amounts.reduce((acc, item) => (acc += item ), 0) .toFixed(2);
 
-    console.log(total)
+    const income = amounts
+                    .filter(item => item > 0)
+                    .reduce((acc, item) => (acc += item), 0)
+                    .toFixed(2);
+
+    const expense = (amounts
+                        .filter(item => item < 0)
+                        .reduce((acc, item) => (acc += item), 0) *
+                        -1
+                      ).toFixed(2);
+    //Inserting balance, income, and expense into the DOM
+    balance.innerText = `$${total}`;
+    money_plus.innerText = `$${income}`;
+    money_minus.innerText = `$${expense}`;
+
 }
 
 //Init app
