a4efd874e5beac690d60105ba89a4d1608173f51
Greg Schuman <gcschuman@hotmail.com>
2020-09-05 17:39:04 -0700

    Fixed the updateValues function.

--- script.js
+++ script.js
@@ -67,27 +67,25 @@ function addTransactionDOM(transaction){
 }
 
 //Update balance, income, and expense
-function updateValues(){
-    const amounts = transaction.map(transaction => transaction.amount);
-
-    const total = amounts.reduce((acc, item) => (acc += item ), 0) .toFixed(2);
-
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction.amount);
+  
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  
     const income = amounts
-                    .filter(item => item > 0)
-                    .reduce((acc, item) => (acc += item), 0)
-                    .toFixed(2);
-
-    const expense = (amounts
-                        .filter(item => item < 0)
-                        .reduce((acc, item) => (acc += item), 0) *
-                        -1
-                      ).toFixed(2);
-    //Inserting balance, income, and expense into the DOM
+      .filter(item => item > 0)
+      .reduce((acc, item) => (acc += item), 0)
+      .toFixed(2);
+  
+    const expense = (
+      amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+      -1
+    ).toFixed(2);
+  
     balance.innerText = `$${total}`;
     money_plus.innerText = `$${income}`;
     money_minus.innerText = `$${expense}`;
-
-}
+  }
 
 //Remove transactions by ID
 function removeTransaction(id){
